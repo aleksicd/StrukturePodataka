532387d7d7b1ccab6338077986df6b38a10fb8f2
David Aleksic <aleksic05david@gmail.com>
2026-09-19 19:53:47 +0200

    Add files via upload

---
+++ BST/Januar 2026/main.cpp
@@ -0,0 +1,20 @@
+#include "BSTreeInt.h"
+
+void main()
+{
+	//TODO: Proveriti rad implementiranog metoda za različite slučajeve izvršenja 
+	BSTreeInt tree;
+	tree.insert(10);
+	tree.insert(5);
+	tree.insert(15);
+	tree.insert(3);
+	tree.insert(7);
+	tree.insert(12);
+	tree.insert(18);
+
+	tree.inorder();
+	cout << endl;
+
+	int suma = tree.sumiraj(7);
+	cout << suma;
+}
